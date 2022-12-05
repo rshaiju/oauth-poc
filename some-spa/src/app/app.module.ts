defdb77990716a55bbbc5cbbf8c7dee46e5e9990
Shaiju Rajan <shaiju.r@falainacloud.com>
2022-12-05 16:15:17 +0800

    added some components and routes

--- some-spa/src/app/app.module.ts
+++ some-spa/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WeatherComponent } from './weather/weather.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WeatherComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
